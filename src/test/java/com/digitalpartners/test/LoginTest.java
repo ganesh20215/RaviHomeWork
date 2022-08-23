3827e2ebe2d55f8efbf330cb7ea98872f205d38b
Ganesh Jadhav <36586236+ganesh20215@users.noreply.github.com>
2022-08-23 07:23:03 +0530

    Update LoginTest.java

--- src/test/java/com/digitalpartners/test/LoginTest.java
+++ src/test/java/com/digitalpartners/test/LoginTest.java
@@ -38,6 +38,7 @@ public class LoginTest extends TestBase {
 		System.out.println("Evening Batch Student");
 		System.out.println("Afternoon Batch Student");
 	    System.out.println("Afternoon Batch Student");
+	    System.out.println("Hello Morning Batch Student");
         softAssert.assertAll();
     }
 
